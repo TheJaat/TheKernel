0d63d5cbe891de93bd96c797e7fb7fbf15aa8394
The Jat <manishkl543@gmail.com>
2025-09-17 20:26:49 +0530

    Don't compile the virtual memory for the time memory as its implementation is pending

--- headers/revision.h
+++ headers/revision.h
@@ -3,12 +3,12 @@
 #ifndef _KERNEL_REVISION_H_
 #define _KERNEL_REVISION_H_
 
-#define BUILD_DATE "19 August 2025"
-#define BUILD_TIME "23:16:40"
+#define BUILD_DATE "17 September 2025"
+#define BUILD_TIME "20:24:45"
 
 #define BUILD_SYSTEM "gcc"
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 2
-#define REVISION_BUILD 130
+#define REVISION_BUILD 132
 
 #endif //!_KERNEL_REVISION_H_
